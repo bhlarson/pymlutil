ef09dbf02e6e8d8b91d4b52fef1b46d1cd5819b1
Brad Larson <bhlarson@gmail.com>
2022-04-28 07:11:02 -0700

    Test files from current path rather than installed module

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 import unittest
 import torch
 
+sys.path.insert(0, os.path.abspath('')) # Test files from current path rather than installed module
 from pymlutil.functions import *
 
 
@@ -15,3 +16,6 @@ from pymlutil.functions import *
 class Test(unittest.TestCase):
     def test_GaussianBasis(self):
         self.assertEqual(GaussianBasis(torch.tensor(0.0), zero=0.0, sigma=0.33), 1.0)
+
+if __name__ == '__main__':
+    unittest.main()
